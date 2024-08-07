51b349d1a608879dc1cc5e75283dfe147514e589
Karina Bianek <bianekkarina@gmail.com>
2024-08-07 16:57:30 -0300

    coisa nova

--- usuarios.js
+++ usuarios.js
@@ -1,5 +1,5 @@
 module.exports= [
-    { id: 1, nome: 'Alice', telefone: '1234-5678', email: 'alice@example.com' },
-    { id: 2, nome: 'Bruna', telefone: '8765-4321', email: 'bruna@example.com' },
-    { id: 3, nome: 'Carol', telefone: '5678-1234', email: 'carol@example.com' }
+    { id: 1, nome: 'Alice', telefone: ['8765-4321', '1232-3421'], email: 'alice@example.com' },
+    { id: 2, nome: 'Bruna', telefone: ['8765-4321'], email: 'bruna@example.com' },
+    { id: 3, nome: 'Carol', telefone: ['5678-1234', '5885-8484'], email: 'carol@example.com' }
 ]
