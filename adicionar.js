d6fff42fb25b11b63d867e0a68f0a5fecf0fe7d0
Karina Bianek <bianekkarina@gmail.com>
2024-08-07 16:58:03 -0300

    coisa nova

--- adicionar.js
+++ adicionar.js
@@ -2,7 +2,13 @@ let usuarios = require('./usuarios')
 
 function adicionarUsuario(usuario) {
     usuario.id = usuarios.length + 1
+
+    let jaExiste = usuarios.find(usu => usu.email === usuario.email)
+    if (jaExiste) {
+        throw new Error("Já existe um usuário com esse email!")
+    } else {
     usuarios.push(usuario)
+    }
 }
 
 module.exports = adicionarUsuario
