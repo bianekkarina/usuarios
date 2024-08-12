208424461b6bb55d8af8818ea025af65f8d37ab6
Karina Bianek <bianekkarina@gmail.com>
2024-08-12 16:16:22 -0300

    mudanças

--- editar.js
+++ editar.js
@@ -1,9 +1,17 @@
-let usuarios = require('./usuarios')
+let usuarios = require('./usuarios');
 
 function editarUsuario(id, novoUsuario) {
+
+   let mesmoEmail = usuarios.find(usuario => usuario.email === novoUsuario.email)
+   if (mesmoEmail) {
+      console.log("Erro: já existe um usuário com esse email!")
+      return false
+   }
+
    let index = usuarios.findIndex(usuario => usuario.id === id) 
    if (index !== -1) {
     usuarios[index] = {id: id, ...novoUsuario}
+    console.log('Usuário atualizado com sucesso!');
    }
 }
 
