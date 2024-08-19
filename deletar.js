f9ffd266284a353a5f79a321aae2ae059aaf23bf
Karina Bianek <bianekkarina@gmail.com>
2024-08-19 15:57:40 -0300

    Update deletar.js

--- deletar.js
+++ deletar.js
@@ -5,7 +5,7 @@ function deletarUsuario(id, confirmacao) {
     if (index !== -1) {
         if (confirmacao.toLowerCase() === 'sim') {
             usuarios.splice(index, 1)
-            console.log('Usuário deletado com sucesso!')
+            console.log('Usuário deletado com sucesso.')
             return true
         } else {
             return false
